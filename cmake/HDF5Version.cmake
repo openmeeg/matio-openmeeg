322ea62d586b61aabd6d16e251f1bf36a5e0de5a
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-03-21 08:38:35 +0100

    Make the error message fatal. Added a debug message.

--- cmake/HDF5Version.cmake
+++ cmake/HDF5Version.cmake
@@ -9,8 +9,9 @@ function(HDF5_VERIFY_VERSION HDF5MINVERS HDF5_COMP_RES)
             ${CMAKE_CURRENT_SOURCE_DIR}/cmake/HDF5Version.c
             COMPILE_DEFINITIONS ${DEFS}
             RUN_OUTPUT_VARIABLE HDF5VERS)
+    message("::: ${RRESULT} ::: ${CRESULT} ::::")
     if (NOT ${CRESULT})
-        message(ERROR "Unable to compile a simple hdf5 program. Check your installation.")
+        message(FATAL "Unable to compile a simple hdf5 program. Check your installation.")
     endif()
     if (NOT ${RRESULT} EQUAL 0)
         message(ERROR "Executing a simple hdf5 program.")
