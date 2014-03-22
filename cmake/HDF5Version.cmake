fc383b3594bbbcd2d37fe6f927048b1a28536137
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-03-22 11:51:53 +0100

    Remove error message and non useful lines.

--- cmake/HDF5Version.cmake
+++ cmake/HDF5Version.cmake
@@ -1,15 +1,10 @@
 include(CompareVersionStrings)
 function(HDF5_VERIFY_VERSION HDF5MINVERS HDF5_COMP_RES)
-    set(DEFS ${HDF5_DEFINITIONS})
-    foreach(i ${HDF5_INCLUDE_DIRS})
-        set(DEFS ${DEFS} -I ${i})
-    endforeach()
     try_run(RRESULT CRESULT
             ${CMAKE_BINARY_DIR}/cmake
             ${CMAKE_CURRENT_SOURCE_DIR}/cmake/HDF5Version.c
             CMAKE_FLAGS -DINCLUDE_DIRECTORIES::STRING=${HDF5_INCLUDE_DIRS}
             RUN_OUTPUT_VARIABLE HDF5VERS)
-    message("::: ${RRESULT} ::: ${CRESULT} ::::")
     if (NOT ${CRESULT})
         message(FATAL "Unable to compile a simple hdf5 program. Check your installation.")
     endif()
